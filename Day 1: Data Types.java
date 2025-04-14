a8ebfaf646f7d99a048fa98b3a0005b3f984393a
Madhesh H <144439699+madddx@users.noreply.github.com>
2025-04-14 19:46:10 +0530

    Create Day 1: Data Types.java

---
+++ Day 1: Data Types.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = 4;
+        double b = 4.0;
+        String c = "HackerRank ";
+    
+        int d=sc.nextInt();
+        System.out.println(a+d);
+        
+        double e = sc.nextDouble();
+        System.out.println(b+e);
+        sc.nextLine();
+        
+        String f = sc.nextLine();
+        String g = c+f;
+        System.out.println(g);
+        
+        sc.close();
+        
+        
+        
+        
+}
+}
